66024ee3acf7a9d3cee563b53e16bf94bc555b75
Konsult <Konsult@LEXSMAKON2>
2017-04-27 11:01:35 +0200

    Add project files.

---
+++ GitHub_Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHub_Demo.Startup))]
+namespace GitHub_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
